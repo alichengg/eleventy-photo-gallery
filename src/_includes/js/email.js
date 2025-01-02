bcb17f1053727636361a8b4fa358b59d462d996e
alichengg <18875813391@163.com>
2025-01-02 21:49:26 +1300

    Update email.js

--- src/_includes/js/email.js
+++ src/_includes/js/email.js
@@ -1,5 +1,5 @@
 // update this variable with your email :)
-const email = "foobar@gmail.com";
+const email = "alicheng96@gmail.com";
 const submit = document.querySelector(".reach-me");
 
 function sendMail() {
